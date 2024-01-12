234cd09671c35b7bf077ebd53a0d567b2c4a42d6
jackiezzhen <jackiezhen31@gmail.com>
2024-01-12 09:24:48 -0500

    s

--- create/js/main.js
+++ create/js/main.js
@@ -1,16 +1,38 @@
 import { arrCakes } from "./arr.js";
 const DOMSelectors = {
+  optionSizes: document.querySelectorAll(),
   optionLarge: document.querySelector("button.optionLarge"),
   optionMedium: document.querySelector("button.optionMedium"),
   optionSmall: document.querySelector("button.optionSmall"),
   stack: document.querySelector(".stackContainer"),
+  form: document.querySelector(".form"),
 };
-function chooseLayers() {
-  optionLarge.addEventListener("click");
-  const layerHTML = `<form action="form" class="form">        
-<label for="layer">Designate the Number of Layers</label>
-<input type="number" id="quantity" name="quantity" min="1">
-    </form>`;
+
+function selectSize() {
+  DOMSelectors.form.addEventListener("submit", function(event){
+    const sizeHTML = `<h1>Please Select A Cake Size</h1>
+    <button class="optionLarge"><img class=largeButton src="" alt="Large Cake"></button>
+    <button class="optionMedium"><img class=mediumButton src="" alt="Medium Cake"></button>
+    <button class="optionSmall"><img class=smallButton src="" alt="Small Cake"></button>`;
+    DOMSelectors.stack.insertAdjacentHTML("beforeend", sizeHTML);
+  });
+
 }
 
+function chooseLayers() {
+  function optionLarge(){
+
+  }
+  function optionMedium(){
+
+  }
+  function optionSmall(){
+
+  }
+}
 function generateLayers() {}
+
+
+function cakeInfo(){
+
+}
