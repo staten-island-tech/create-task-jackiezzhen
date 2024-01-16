b028b67d037293aaec916688598ef0eaa17708af
jackiezzhen <jackiezhen31@gmail.com>
2024-01-16 09:25:07 -0500

    quik

--- create/js/arr.js
+++ create/js/arr.js
@@ -1,55 +1,26 @@
-const arrCakes = [
-    {
-    flavor:"",
-    priceLarge:"",
-    priceMedium:"",
-    priceSmall:"",
-    ingredients:"",
-    
-    imgLarge:"",
-    imgMedium:"",
-    imgSmall:"",
-},
-{
-    flavor:"",
-    priceLarge:"",
-    priceMedium:"",
-    priceSmall:"",
-    ingredients:"",
-    imgLarge:"",
-    imgMedium:"",
-    imgSmall:"",
-},
-{
-    flavor:"",
-    priceLarge:"",
-    priceMedium:"",
-    priceSmall:"",
-    ingredients:"",
-    imgLarge:"",
-    imgMedium:"",
-    imgSmall:"",
-},
-{
-    flavor:"",
-    priceLarge:"",
-    priceMedium:"",
-    priceSmall:"",
-    ingredients:"",
-    imgLarge:"",
-    imgMedium:"",
-    imgSmall:"",
-},
-{
-    flavor:"",
-    priceLarge:"",
-    priceMedium:"",
-    priceSmall:"",
-    ingredients:"",
-    imgLarge:"",
-    imgMedium:"",
-    imgSmall:"",
-},
-]
-
-export {arrCakes}
+const cakeArr = [
+  {
+    flavor: "Choco Cloud",
+    large: {
+      priceLarge: "",
+      imgLarge: "",
+      imgLayerLarge: "",
+    },
+    medium: {
+      priceMedium: "",
+      imgMedium: "",
+      imgLayerMedium: "",
+    },
+    small: {
+      priceSmall: "",
+      imgSmall: "",
+      imgLayerSmall: "",
+    },
+    ingredients: ["Cloud cream[heavy cream(milk), whipping cream, sugar]", 
+    "Choco cake sheet(liquid egg, sugar, wheat flour, butter, dark cocoa powder, dough improver, baking powder, sea salt), Semisweet chocolate, Strawberry, Simple syrup (water, sugar, lemon)", 
+    "Cocoa powder", 
+    "Semisweet chocolate"],
+  },
+ 
+];
+export { cakeArr };
